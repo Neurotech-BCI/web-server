7970b0b918c0e10e1b9e57f5f52bdd4225c4f10b
AsherHoltham <asher.holtham.github@gmail.com>
2025-04-12 22:04:40 -0700

    web build update

--- web-build/flutter_bootstrap.js
+++ web-build/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3260344783"
+    serviceWorkerVersion: "2927607238"
   }
 });
