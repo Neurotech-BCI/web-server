5aae92aa80035244df68469f0bd0e3af11ac5f48
AsherHoltham <asher.holtham.github@gmail.com>
2025-04-14 21:30:33 -0700

    new test sequence for web build

--- web-build/flutter_service_worker.js
+++ web-build/flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "3e6f2cf09f135a13141443f5af80ff4a",
+const RESOURCES = {"flutter_bootstrap.js": "6ba9fd5e940b68977d526b3399b4d25b",
 "version.json": "2b521e10dfa0f067561de489a19d6620",
 "index.html": "7fcdddaf0b8a074bafd2a57159bb8906",
 "/": "7fcdddaf0b8a074bafd2a57159bb8906",
-"main.dart.js": "189af4b09478ddb637bb2ac6d9e5f9a4",
+"main.dart.js": "f644ae567e85a960f03809ece81af4d8",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
 "favicon.png": "5637eb0c41bf41fc07b9199cbae69f98",
 "icons/logo-512.png": "76dc83d00c1dc15025751c8a3737ef92",
