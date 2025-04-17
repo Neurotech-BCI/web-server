bf644f7488f2e7b0de10727e063021af623400ce
AsherHoltham <asher.holtham.github@gmail.com>
2025-04-17 13:17:08 -0700

    web build update

--- web-build/flutter_service_worker.js
+++ web-build/flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "732000db792801eecbe0ea80c6601f7b",
+const RESOURCES = {"flutter_bootstrap.js": "10140e5816ed09e4ee3ef6abd1871776",
 "version.json": "2b521e10dfa0f067561de489a19d6620",
 "index.html": "7fcdddaf0b8a074bafd2a57159bb8906",
 "/": "7fcdddaf0b8a074bafd2a57159bb8906",
-"main.dart.js": "fa48bd26ace3dac006be97fa792d9d1e",
+"main.dart.js": "51beebf639d8850d61c277c483ba412f",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
 "favicon.png": "5637eb0c41bf41fc07b9199cbae69f98",
 "icons/logo-512.png": "76dc83d00c1dc15025751c8a3737ef92",
